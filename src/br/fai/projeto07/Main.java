d6efecf5f5d008d250d1d7ef46bba76799a109ed
Guilherme <guisrsk8@gmail.com>
2020-03-08 00:28:42 -0300

    primeiro commit

---
+++ src/br/fai/projeto07/Main.java
@@ -0,0 +1,26 @@
+package br.fai.projeto07;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Main app = new Main();
+		app.start();
+
+	}
+
+	private void start() {
+		operacao();
+		
+	}
+	
+	private void operacao(){
+			
+		
+	}
+
+	
+}
