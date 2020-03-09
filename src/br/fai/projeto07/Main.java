f5c78545e0bd0a93531701a5ad9c92f54538d0fb
Guilherme <guisrsk8@gmail.com>
2020-03-08 23:17:16 -0300

    commit teste

--- src/br/fai/projeto07/Main.java
+++ src/br/fai/projeto07/Main.java
@@ -2,25 +2,28 @@ package br.fai.projeto07;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		
+
 		Main app = new Main();
 		app.start();
 
 	}
 
 	private void start() {
+		
+		
 		operacao();
-		
-	}
-	
-	private void operacao(){
-			
-		
+
 	}
 
-	
+	private void operacao(int x, int y) {
+		int x, y;
+		x = 5;
+		y = 5;
+		int soma = x + y;
+		
+
+		System.out.println("soma: " + soma);
+
+	}
 }
